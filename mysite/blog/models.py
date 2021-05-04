98a16a0820a79f6446dff04d50d99c5ea51f61a3
TruongCL <truongcl@eikkeisoft.com>
2021-05-04 23:14:58 +0700

    .

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
                      self).get_queryset() \
-                        .filter(status='published')
+            .filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -36,3 +37,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
