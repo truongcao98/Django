aa98b264a8e0c72106d6ef657d238756ee645973
TruongCL <truongcl@eikkeisoft.com>
2021-05-05 11:27:40 +0700

    chapter 2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from pip._internal import req
+
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 def post_detail(request, year, month, day, post):
@@ -19,6 +22,18 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
 
 
 class PostListView(ListView):
