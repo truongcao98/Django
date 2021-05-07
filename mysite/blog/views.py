e28bf3ebf7b1a14faa7d585007ee138afce7242d
kutru <kutruong05031998@gmail.com>
2021-05-07 16:29:42 +0700

    finish chapter 2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post, Comment
@@ -29,21 +29,27 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # list of similar_posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('same_tag', 'publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
-def post_list(request, tag_slug):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
 
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list=object_list.filter(tags_in=[tag])
+        object_list = object_list.filter(tags_in=[tag])
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
